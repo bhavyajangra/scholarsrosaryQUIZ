856f70824d8bace550d85f72dcd27da40eaa1883
bhavyajangra <bhavyajangra1712@gmail.com>
2024-10-04 23:05:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,45 +9,4 @@ function generateOptions(options) {
     const optionsHtml = options.map((option, index) => {
         return `
             <li>
-                <input type="radio" id="option-${index}" name="option" value="${option}">
-                <label for="option-${index}">${option}</label>
-            </li>
-        `;
-    }).join('');
-    document.getElementById('options').innerHTML = optionsHtml;
-}
-
-// function to display question and options
-function displayQuestion() {
-    const question = quizData[currentQuestion].question;
-    const options = quizData[currentQuestion].options;
-    document.getElementById('question').innerHTML = question;
-    generateOptions(options);
-}
-
-// function to check answer
-function checkAnswer() {
-    const userAnswer = document.querySelector('input[name="option"]:checked').value;
-    const correctAnswer = quizData[currentQuestion].correctAnswer;
-    if (userAnswer === correctAnswer) {
-        document.getElementById('result').innerHTML = 'Correct!';
-    } else {
-        document.getElementById('result').innerHTML = `Incorrect. The correct answer is ${correctAnswer}.`;
-    }
-}
-
-// function to display next question
-function displayNextQuestion() {
-    currentQuestion++;
-    if (currentQuestion >= quizData.length) {
-        currentQuestion = 0;
-    }
-    displayQuestion();
-}
-
-// event listeners
-document.getElementById('submit-btn').addEventListener('click', checkAnswer);
-document.getElementById('next-btn').addEventListener('click', displayNextQuestion);
-
-// initialize quiz
-displayQuestion();
+                <input type="radio" id="option-${index}" name
